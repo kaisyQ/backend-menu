0a0edf959fd8ed18965d9d7d8ce1333be2dbc139
Kaisy <rutyo8@mail.ru>
2022-07-05 19:26:35 +1100

    db-scheme

--- index.ts
+++ index.ts
@@ -1,8 +1,16 @@
 import express, { Express, Request, Response } from "express"
 
+import { PrismaClient } from '@prisma/client'
+const prisma: PrismaClient = new PrismaClient()
+
 const app: Express = express()
 
-app.get('/', (req: Request, res: Response) => {
+app.get('/', async (req: Request, res: Response) => {
+    /*await prisma.categories.create({
+        data: {
+            name: 'Coffee'
+        }
+    })*/
     res.send(`
         <h1>this is express + ts app</h1>
     `)
